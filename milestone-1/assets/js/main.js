39bd87ecd91a96b1f9b1305dc7c3b831229f9bfa
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-03 10:30:04 +0100

    Creato file javascript (milestone-1)
    Inserita solo la traccia in formato commento.

---
+++ milestone-1/assets/js/main.js
@@ -0,0 +1,12 @@
+/* 
+Leggete attentamente la brief di progetto e cominciate a lavorare ad una milestone alla volta.
+Ogni milesone va in una cartella dedicata es: vue-boolzapp/milesone-1
+
+Boolzapp - a (not very) innovative messaging platform
+
+Milestone 1:
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse;
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+nome e immagine di ogni contatto.
+*/
